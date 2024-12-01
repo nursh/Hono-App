ac8280f25b075fafacf570e906dddde31b830934
nur-sh <nsheikhmohammed01@gmail.com>
2024-11-30 23:02:50 -0500

    Separate App creation logic to its own file

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,6 @@
-import { OpenAPIHono } from "@hono/zod-openapi";
-import { notFound, onError } from "stoker/middlewares";
-import { pinoLogger } from "./middlewares/pino-logger.js";
-import type { PinoLogger } from "hono-pino";
+import createApp from "./lib/create-app.js";
 
-
-interface AppBindings {
-  Variables: {
-    logger: PinoLogger
-  }
-}
-
-const app = new OpenAPIHono<AppBindings>();
-app.use(pinoLogger());
+const app = createApp();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
@@ -21,8 +10,5 @@ app.get('/error', (c) => {
   c.status(422);
   throw new Error("Something bad happened here")
 })
-
-app.notFound(notFound)
-app.onError(onError);
 
 export default app;
