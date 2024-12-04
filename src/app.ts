5ad001bec0183069b302496375ea0681076990a5
nur-sh <nsheikhmohammed01@gmail.com>
2024-12-03 21:39:31 -0500

    Add basic route for openAPI

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import configureOpenAPI from "./lib/configure-open-api.js";
 import createApp from "./lib/create-app.js";
+import index from '@/routes/index.routes.js';
 
 const app = createApp();
+
+const routes = [
+  index
+];
+
 configureOpenAPI(app);
+routes.forEach((route) => {
+  app.route('/', route);
+})
 
 export default app;
