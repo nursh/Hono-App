31b189aac78d4eabb517feb23dcf4f5e5fd70d62
nur-sh <nsheikhmohammed01@gmail.com>
2024-12-03 21:58:43 -0500

    Add scalar specification for OpenAPI

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,5 +1,6 @@
 import type { AppOpenAPI } from "./types.js";
 import packageJSON from  "../../package.json" assert { type: "json" };
+import { apiReference } from "@scalar/hono-api-reference";
 
 export default function configureOpenAPI(app: AppOpenAPI) {
   app.doc("/doc", {
@@ -8,5 +9,20 @@ export default function configureOpenAPI(app: AppOpenAPI) {
       version: packageJSON.version,
       title: "Tasks API"
     }
-  })
+  });
+
+  app.get(
+    '/reference',
+    apiReference({
+      theme: 'kepler',
+      layout: 'classic',
+      defaultHttpClient: {
+        targetKey: 'javascript',
+        clientKey: 'fetch'
+      },
+      spec: {
+        url: '/doc',
+      },
+    }),
+  )
 }
